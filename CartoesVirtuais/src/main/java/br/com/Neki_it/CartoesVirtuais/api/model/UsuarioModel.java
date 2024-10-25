61e15fa8b6907b7604612469f48fe951b7995a93
Pabullo-M <pabullo.muniz@gmail.com>
2024-10-25 10:17:30 -0300

    feat: Criando estrutura inicial do projeto com UsuarioModel

---
+++ CartoesVirtuais/src/main/java/br/com/Neki_it/CartoesVirtuais/api/model/UsuarioModel.java
@@ -0,0 +1,27 @@
+package br.com.Neki_it.CartoesVirtuais.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+public class UsuarioModel {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nome;
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String senha;
+	
+}
---
+++ CartoesVirtuais/src/test/java/br/com/Neki_it/CartoesVirtuais/CartoesVirtuaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Neki_it.CartoesVirtuais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoesVirtuaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
