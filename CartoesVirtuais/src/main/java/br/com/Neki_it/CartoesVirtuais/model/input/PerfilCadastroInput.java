27d7083d661a63791a0256d7a8971e8e2e68e8c9
Pabullo-M <pabullo.muniz@gmail.com>
2024-10-25 11:39:18 -0300

    feat cria a classe PerfilCadastroInput, no package br.com.Neki_it.CartoesVirtuais.model.input.

---
+++ CartoesVirtuais/src/main/java/br/com/Neki_it/CartoesVirtuais/model/input/PerfilCadastroInput.java
@@ -0,0 +1,41 @@
+package br.com.Neki_it.CartoesVirtuais.api.model.input;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class PerfilCadastroInput {
+
+	
+	@NotBlank
+	private String email;
+	@Schema(example = "Arthur Nunes")
+	@NotBlank
+	private String nomeCompleto;
+	
+	@Schema(example = "Zico")
+	private String nomeSocial;
+	
+	@Schema(example = "03/03/1953")
+	@NotBlank
+	private String dataNascimento;
+	
+	@NotBlank
+	private String foto;
+	
+	@Schema(example = "(021)912345678")
+	private String telefone;
+	
+	private String linkedin;
+	
+	private String github;
+	
+	private String instagram;
+	
+	private String facebook;
+	
+}
