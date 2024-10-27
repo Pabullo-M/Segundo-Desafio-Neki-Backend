dd2803dbf20379afdc1866c7eceb72fad0eaadc0
Pabullo-M <pabullo.muniz@gmail.com>
2024-10-27 15:41:27 -0300

    fix: Ajusta getters, setters e constructors da classe PerfilModel para escrito manualmente, retirando o Lombok

--- CartoesVirtuais/src/main/java/br/com/Neki_it/CartoesVirtuais/model/PerfilModel.java
+++ CartoesVirtuais/src/main/java/br/com/Neki_it/CartoesVirtuais/model/PerfilModel.java
@@ -1,5 +1,6 @@
 package br.com.Neki_it.CartoesVirtuais.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,8 +10,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
+
 @Entity
 public class PerfilModel {
 	
@@ -20,6 +20,7 @@ public class PerfilModel {
 	private Long id;
 	
 	@NotBlank
+	@Column(unique = true)
 	private String email;
 	
 	@NotBlank
@@ -37,4 +38,87 @@ public class PerfilModel {
 	
 	@Embedded
 	private RedesSociaisModel redesSociais;
+	
+	public PerfilModel() {
+		super();
+	}
+
+	public PerfilModel(Long id, @NotBlank String email, @NotBlank String nomeCompleto, String nomeSocial,
+			@NotBlank String dataNascimento, @NotBlank String foto, String telefone, RedesSociaisModel redesSociais) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.nomeCompleto = nomeCompleto;
+		this.nomeSocial = nomeSocial;
+		this.dataNascimento = dataNascimento;
+		this.foto = foto;
+		this.telefone = telefone;
+		this.redesSociais = redesSociais;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getNomeSocial() {
+		return nomeSocial;
+	}
+
+	public void setNomeSocial(String nomeSocial) {
+		this.nomeSocial = nomeSocial;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public RedesSociaisModel getRedesSociais() {
+		return redesSociais;
+	}
+
+	public void setRedesSociais(RedesSociaisModel redesSociais) {
+		this.redesSociais = redesSociais;
+	}
+	
+	
 }
